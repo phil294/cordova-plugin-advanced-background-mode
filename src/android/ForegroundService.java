cb2a92ee1048ee48309a1d1a5cd41fb2011cdea4
AlcanceTec <87829827+AlcanceTec@users.noreply.github.com>
2023-05-17 11:43:35 -0300

    Update ForegroundService.java

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -199,6 +199,10 @@ public class ForegroundService extends Service {
 
         if (intent != null && settings.optBoolean("resume")) {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            // PendingIntent contentIntent = PendingIntent.getActivity(
+            //         context, NOTIFICATION_ID, intent,
+            //         PendingIntent.FLAG_UPDATE_CURRENT);
+            
             PendingIntent contentIntent = PendingIntent.getActivity( 
                 context, NOTIFICATION_ID, intent, 
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
